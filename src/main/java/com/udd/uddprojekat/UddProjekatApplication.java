d442adeb57343282d2eedfe0fb9eb76ef6d40f63
Nadja Gvozdenac <gvozdenacn@gmail.com>
2024-02-03 12:16:12 +0100

    Init project

---
+++ src/main/java/com/udd/uddprojekat/UddProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.udd.uddprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UddProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UddProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udd/uddprojekat/UddProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udd.uddprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UddProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
